69fe789c50cace89d0ffdf6516b2025d26b52cb5
Thiago <thiagonascimentodeveloper@gmail.com>
2022-07-28 15:11:32 -0300

    iniciando o projeto to do

---
+++ src/pages/App/index.tsx
@@ -0,0 +1,13 @@
+import * as C from './styles';
+
+export const App = () => {
+  return (
+    <div className="container">
+      <C.Container>
+        <C.AreaApp>
+          <C.Header>Lista de Tarefas</C.Header>
+        </C.AreaApp>
+      </C.Container>
+    </div>
+  )
+}
---
+++ src/pages/App/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    background-color: #17181f;
+    color: #797a81;
+    min-height: 100vh;
+`;
+
+export const AreaApp = styled.div`
+    max-width: 992px;
+    margin: auto;
+    padding: 10px;
+`;
+
+export const Header = styled.h1`
+    color: #fff;
+    border-bottom: 1px solid #444;
+    padding-bottom: 20px;
+    text-align: center;
+`;
